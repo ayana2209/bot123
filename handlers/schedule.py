5d77af274e7e518ef8b0e6481cdbd7a0b0f873f0
ayana2209 <126326335+ayana2209@users.noreply.github.com>
2023-04-03 11:26:11 +0600

    Add files via upload

---
+++ handlers/schedule.py
@@ -0,0 +1,25 @@
+import datetime
+
+from aiogram import types
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.triggers.date import DateTrigger
+from config import bot
+
+
+async def notification_id(message: types.Message):
+    user_ids = [message.from_user.id]
+    for user_id in user_ids:
+        await message.answer(user_id[0], "Поздровляю, ты дожил до каникул ＼(￣▽￣)／!")
+
+
+async def set_scheduler():
+    scheduler = AsyncIOScheduler(timezone='Asia/Bishkek')
+    scheduler.add_job(
+    notification_id,
+        trigger=DateTrigger(
+            run_date=datetime.datatime(year=2023, month=6, day=1, hour=8, minute=30)
+        ),
+        kwargs={"message": bot},
+    )
+
+    scheduler.start()
